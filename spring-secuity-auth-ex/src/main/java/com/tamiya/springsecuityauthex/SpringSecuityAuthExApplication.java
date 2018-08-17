73b21234142795ae680cc274dc98fdbad8f29099
s-tamiya <sy.tamiya@gmail.com>
2018-08-17 09:27:40 +0900

    first commit

---
+++ spring-secuity-auth-ex/src/main/java/com/tamiya/springsecuityauthex/SpringSecuityAuthExApplication.java
@@ -0,0 +1,12 @@
+package com.tamiya.springsecuityauthex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuityAuthExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuityAuthExApplication.class, args);
+	}
+}

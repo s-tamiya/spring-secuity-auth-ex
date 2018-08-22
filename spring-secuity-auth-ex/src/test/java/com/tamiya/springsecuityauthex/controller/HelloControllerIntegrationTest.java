2d2b86fb1e96586fa76316d5a9db816ccbb9cd69
s-tamiya <sy.tamiya@gmail.com>
2018-08-22 16:54:49 +0900

    test

---
+++ spring-secuity-auth-ex/src/test/java/com/tamiya/springsecuityauthex/controller/HelloControllerIntegrationTest.java
@@ -0,0 +1,64 @@
+package com.tamiya.springsecuityauthex.controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.nio.charset.Charset;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+// @SpringBootTestを使用する結合テスト
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HelloControllerIntegrationTest {
+
+  // コンテキスト
+  @Autowired
+  private WebApplicationContext context;
+
+  // モックMVCは@beforeでビルドする
+  private MockMvc mvc;
+
+  final private MediaType contentTypeText = new MediaType(
+      MediaType.TEXT_PLAIN.getType(), MediaType.TEXT_PLAIN.getSubtype(), Charset.forName("utf8")
+      );
+
+  @Before
+  public void setup() {
+    // MockMvcBuildersを使用してMockMvcをビルドする
+    mvc = MockMvcBuilders
+            .webAppContextSetup(context) // コンテキストの指定
+            .apply(springSecurity()) // SpringSecurity有効
+            .build(); // ビルド
+  }
+
+  @Test
+  public void greeting() throws Exception {
+    RequestBuilder builder = MockMvcRequestBuilders.get("/hello")
+        .accept(contentTypeText);// contentTypeの設定
+    MvcResult result = mvc.perform(builder)
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(contentTypeText))
+        .andExpect(content().string("hello WORLD"))
+        .andDo(print())
+        .andReturn(); // andReturn()でMvcResultオブジェクトを返す
+
+    assertThat(result.getResponse().getContentAsString()).isEqualTo("HelloWORLD");
+  }
+
+}
